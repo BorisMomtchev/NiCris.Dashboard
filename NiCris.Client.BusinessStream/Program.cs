6625e6cea2cf4929a111fb37f6c9be552c89a872
BorisMomtchev@g.com <BorisMomtchev@g.com>
2012-08-24 00:22:33 -0500

    PostSharp magic

--- NiCris.Client.BusinessStream/Program.cs
+++ NiCris.Client.BusinessStream/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using NiCris.Client.BusinessStream.Aspects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace NiCris.Client.BusinessStream
 {
@@ -9,6 +11,25 @@ namespace NiCris.Client.BusinessStream
     {
         static void Main(string[] args)
         {
+            FirstMethod();
+            SecondMethod();
+
+            Console.WriteLine("\nPress any key to continue");
+            Console.ReadKey();
+        }
+
+        [ExceptionAspect]
+        [LogAspect]
+        static void FirstMethod()
+        {
+            throw new DivideByZeroException("An Error Occured...");
+        }
+
+        [LogAspect]
+        [TimingAspect]
+        static void SecondMethod()
+        {
+            Thread.Sleep(1000);
         }
     }
 }
